6331fa920e75efcaebf4ffb7edee971b5389bea8
adharsh <rjadharsh333@gmail.com>
2023-02-18 17:35:39 +0530

    1.0.5

--- index.js
+++ index.js
@@ -1,141 +1,114 @@
-async function price_in_words(price) {
-  let sglDigit = [
-      'Zero',
-      'One',
-      'Two',
-      'Three',
-      'Four',
-      'Five',
-      'Six',
-      'Seven',
-      'Eight',
-      'Nine',
-    ],
-    dblDigit = [
-      'Ten',
-      'Eleven',
-      'Twelve',
-      'Thirteen',
-      'Fourteen',
-      'Fifteen',
-      'Sixteen',
-      'Seventeen',
-      'Eighteen',
-      'Nineteen',
-    ],
-    tensPlace = [
-      '',
-      'Ten',
-      'Twenty',
-      'Thirty',
-      'Forty',
-      'Fifty',
-      'Sixty',
-      'Seventy',
-      'Eighty',
-      'Ninety',
-    ],
-    handle_tens = function (dgt, prevDgt) {
-      return 0 === dgt
-        ? ''
-        : ' ' + (1 === dgt ? dblDigit[prevDgt] : tensPlace[dgt]);
-    },
-    handle_utlc = function (dgt, nxtDgt, denom) {
-      return (
-        (0 !== dgt && 1 !== nxtDgt ? ' ' + sglDigit[dgt] : '') +
-        (0 !== nxtDgt || dgt > 0 ? ' ' + denom : '')
-      );
-    };
+function convert_number(number) {
+  if (number < 0 || number > 999999999) {
+    return "NUMBER OUT OF RANGE!";
+  }
+  let Gn = Math.floor(number / 10000000);
+  number -= Gn * 10000000;
+  let kn = Math.floor(number / 100000);
+  number -= kn * 100000;
+  let Hn = Math.floor(number / 1000);
+  number -= Hn * 1000;
+  let Dn = Math.floor(number / 100);
+  number = number % 100;
+  let tn = Math.floor(number / 10);
+  let one = Math.floor(number % 10);
+  let res = "";
 
-  let str = '',
-    digitIdx = 0,
-    digit = 0,
-    nxtDigit = 0,
-    words = [];
-  if (((price += ''), isNaN(parseInt(price)))) str = '';
-  else if (parseInt(price) > 0 && price.length <= 10) {
-    for (digitIdx = price.length - 1; digitIdx >= 0; digitIdx--)
-      switch (
-        ((digit = price[digitIdx] - 0),
-        (nxtDigit = digitIdx > 0 ? price[digitIdx - 1] - 0 : 0),
-        price.length - digitIdx - 1)
-      ) {
-        case 0:
-          words.push(handle_utlc(digit, nxtDigit, ''));
-          break;
-        case 1:
-          words.push(handle_tens(digit, price[digitIdx + 1]));
-          break;
-        case 2:
-          words.push(
-            0 !== digit
-              ? ' ' +
-                  sglDigit[digit] +
-                  ' Hundred' +
-                  (0 !== price[digitIdx + 1] && 0 !== price[digitIdx + 2]
-                    ? ' and'
-                    : '')
-              : ''
-          );
-          break;
-        case 3:
-          words.push(handle_utlc(digit, nxtDigit, 'Thousand'));
-          break;
-        case 4:
-          words.push(handle_tens(digit, price[digitIdx + 1]));
-          break;
-        case 5:
-          words.push(handle_utlc(digit, nxtDigit, 'Lakh'));
-          break;
-        case 6:
-          words.push(handle_tens(digit, price[digitIdx + 1]));
-          break;
-        case 7:
-          words.push(handle_utlc(digit, nxtDigit, 'Crore'));
-          break;
-        case 8:
-          words.push(handle_tens(digit, price[digitIdx + 1]));
-          break;
-        case 9:
-          words.push(
-            0 !== digit
-              ? ' ' +
-                  sglDigit[digit] +
-                  ' Hundred' +
-                  (0 !== price[digitIdx + 1] || 0 !== price[digitIdx + 2]
-                    ? ' and'
-                    : ' Crore')
-              : ''
-          );
+  if (Gn > 0) {
+    res += convert_number(Gn) + " CRORE";
+  }
+  if (kn > 0) {
+    res += (res == "" ? "" : " ") + convert_number(kn) + " LAKH";
+  }
+  if (Hn > 0) {
+    res += (res == "" ? "" : " ") + convert_number(Hn) + " THOUSAND";
+  }
+
+  if (Dn) {
+    res += (res == "" ? "" : " ") + convert_number(Dn) + " HUNDRED";
+  }
+
+  let ones = Array(
+    "",
+    "ONE",
+    "TWO",
+    "THREE",
+    "FOUR",
+    "FIVE",
+    "SIX",
+    "SEVEN",
+    "EIGHT",
+    "NINE",
+    "TEN",
+    "ELEVEN",
+    "TWELVE",
+    "THIRTEEN",
+    "FOURTEEN",
+    "FIFTEEN",
+    "SIXTEEN",
+    "SEVENTEEN",
+    "EIGHTEEN",
+    "NINETEEN"
+  );
+  let tens = Array(
+    "",
+    "",
+    "TWENTY",
+    "THIRTY",
+    "FOURTY",
+    "FIFTY",
+    "SIXTY",
+    "SEVENTY",
+    "EIGHTY",
+    "NINETY"
+  );
+
+  if (tn > 0 || one > 0) {
+    if (!(res == "")) {
+      res += " AND ";
+    }
+    if (tn < 2) {
+      res += ones[tn * 10 + one];
+    } else {
+      res += tens[tn];
+      if (one > 0) {
+        res += "-" + ones[one];
       }
-    str = words.reverse().join('');
-  } else str = '';
-  console.log(str)
-  return str;
-}
-
-exports.price_in_rupees = async function(total) {
-  let splittedNum = total.toString().split('.');
-  let nonDecimal = splittedNum[0];
-  let decimal = splittedNum[1] ? splittedNum[1] : 0;
-  if (decimal[0] === '0') {
-    if (Number(decimal[1]) > 0) {
-      decimal = '1';
     }
   }
-  if (decimal.length > 2){
-      decimal = decimal.split('')
-      decimal[2] = `${Number(decimal[2]) + 1}` ? Number(decimal[2]) > 5 : Number(decimal[2])
-      decimal = decimal.join('')
-      decimal = decimal.substring(0,2)
-  }
-  let value =
-    price_in_words(Number(nonDecimal)) +
-    'Rupees and' +
-    price_in_words(Number(decimal)) +
-    'Paise';
 
-  let num = nonDecimal + '.' + decimal;
-  return [value, num];
+  if (res == "") {
+    res = "zero";
+  }
+  return res;
 }
 
+exports.currency_in_wordsinr = function (value) {
+  let fraction = Math.round((value % 1) * 100);
+  let fraction_text = "";
+
+  let splitNum = value.toString().split(".");
+
+  if (splitNum[1].length > 2) {
+    let decimal = splitNum[1];
+    decimal = decimal.split("");
+    decimal[1] =
+      Number(decimal[2]) > 5 ? `${Number(decimal[1]) + 1}` : Number(decimal[1]);
+
+    decimal = decimal.join("");
+    decimal = decimal.substring(0, 2);
+    splitNum[1] = decimal;
+  }
+
+  splitNum = splitNum.join(".");
+  let currency_in_words = convert_number(value) + " RUPEES";
+
+  if (fraction > 0) {
+    fraction_text = " AND " + convert_number(fraction) + " PAISE";
+    currency_in_words += fraction_text;
+  }
+
+  currency_in_words += " ONLY";
+
+  return [currency_in_words, splitNum];
+};
